58a724de17ad46861d4ce02eaa2afba854214463
groomigroom <googjohn0143@gmail.com>
2026-03-16 22:25:42 +0000

    v6before

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import './App.css';
 import Header_part from "./component/Header_part";
 import DayList from './component/DayList';
 import Day from './component/Day';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header_part />
-        <Switch>
-          <Route path="/">
-            <DayList />
-          </Route>
-          <Route path="/day">
-            <Day />
-          </Route>
-
-
-        </Switch>
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day" element={<Day />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+//v6 방식 찾아보기 route

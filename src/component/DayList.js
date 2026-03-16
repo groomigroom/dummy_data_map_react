0208994963b5cbc0d3a1857514378778653940ad
groomigroom <googjohn0143@gmail.com>
2026-03-16 21:43:29 +0000

    ul_edit_part

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -3,14 +3,12 @@ import dummy from '../db/data.json';
 export default function DayList() {
     console.log(dummy);
     return (
-        <div>
-            <ul className='list_day'>
+        <ul className='list_day'>
                 {dummy.days.map(day => (
                     <li>
                         Day {day.day}
                     </li>
                 ))} 
-            </ul>;
-        </div>
+            </ul>
     );
 }
